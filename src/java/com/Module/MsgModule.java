c7fb84af972cb0b9271fa9fed873c887f8d9d7c4
13301144@bjtu.edu.cn <13301144@bjtu.edu.cn>
2016-04-11 11:08:17 +0800

    新增了MsgModule,PostingModule等4个module

---
+++ src/java/com/Module/MsgModule.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Module;
+
+
+import Hibernate.msg;
+import Hibernate.user;
+import HibernateUtil.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author bai
+ */
+public class MsgModule {
+    //插入消息
+	public static void Msg(String msg,String type,String sender_id,String reader_id){
+                Session s = HibernateUtil.currentSession();
+                HibernateUtil.beginTransaction();
+		String sql = "INSERT into msg(msg,type,sender_id,reader_id,msg_date) "
+				+ "VALUE('" + msg + "','" + type + "','" + sender_id + "','" + reader_id + "',SYSDATE());";
+		int back = s.createSQLQuery(sql).executeUpdate();
+                HibernateUtil.commitTransaction();//结束操作
+                HibernateUtil.closeSession();
+	}
+	
+	//获取用户名
+	public static String getUserName(String user_id){
+                Session s = HibernateUtil.currentSession();
+                HibernateUtil.beginTransaction();
+		String sql = "Select user_name from user"
+				+ " where user_id = "+user_id ;
+		 List<user> list = s.createSQLQuery(sql).list();
+                 HibernateUtil.commitTransaction();//结束操作
+                 HibernateUtil.closeSession();
+
+                 return list.get(0).getUser_name();
+	}
+	
+	//获取用户Id
+	public static String getUserID(String name){
+                Session s = HibernateUtil.currentSession();
+                HibernateUtil.beginTransaction();
+		String sql = "select user_id from user where user_name = '" + name + "'";
+		List<user> list = s.createSQLQuery(sql).list();
+                HibernateUtil.commitTransaction();//结束操作
+                HibernateUtil.closeSession();
+                if(list.isEmpty())
+                    return null;
+                return String.valueOf(list.get(0).getExist());
+	    }
+	
+	//获取某条对话内容
+	public static String getMsg(String Msg_id){
+                Session s = HibernateUtil.currentSession();
+                HibernateUtil.beginTransaction();
+		String sql = "select msg from msg where msg_id = '" + Msg_id + "'";
+		List<msg> list = s.createSQLQuery(sql).list();
+                HibernateUtil.commitTransaction();//结束操作
+                HibernateUtil.closeSession();
+                return list.get(0).getMsg();
+	}
+}
---
+++ src/java/com/Module/SignModule.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Module;
+
+import Hibernate.user;
+import HibernateUtil.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author bai
+ */
+public class SignModule {
+    //签到
+	public static void Sign(String bar_id,String user_id){
+                Session s = HibernateUtil.currentSession();
+                HibernateUtil.beginTransaction();
+		String sql = "update star_bar set sign_num = sign_num+1"
+				+ " where bar_id = '" + bar_id + "' and user_id = '" + user_id + "'";
+		int back = s.createSQLQuery(sql).executeUpdate();
+		sql = "update star_bar set sign_date = SYSDATE() "
+				+ " where bar_id = '" + bar_id + "' and user_id = '" + user_id + "'";
+		int back1 = s.createSQLQuery(sql).executeUpdate();
+                HibernateUtil.commitTransaction();//结束操作
+                HibernateUtil.closeSession();
+	}
+	//获得用户ID
+	public static String getUserID(String name){
+                Session s = HibernateUtil.currentSession();
+                HibernateUtil.beginTransaction();
+		String sql = "select user_id from user where user_name = '" + name + "'";
+		List<user> list = s.createSQLQuery(sql).list();
+		HibernateUtil.commitTransaction();//结束操作
+                HibernateUtil.closeSession();
+		return String.valueOf(list.get(0).getUser_id());
+	}
+}
