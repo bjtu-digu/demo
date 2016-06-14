0e8c08f7758814acf4ac6202215b513360d3aa0d
dong hao <13301147@bjtu.edu.cn>
2016-06-14 14:53:12 +0800

    del ctrl error code

--- src/java/com/Control/BarCtrl.java
+++ src/java/com/Control/BarCtrl.java
@@ -29,89 +29,88 @@ public class BarCtrl extends Ctrl{
 		PostList = BarModule.getPostList(String.valueOf(BarInfo.get(0).getBar_id()), 
 				((pageNum-1)*10+1) + "", ((pageNum-1)*10+10) + "");
 	}
-	//������
 	public boolean CheckBarName(){
 		return BarModule.checkBar(name) != 1;
 	}
 
 	
 /////////////////////////
-//���������
+//
 /////////////////////////
-	//��ȡ����id
+	//
 	public String getBarOwnerId(){
 		return String.valueOf(BarInfo.get(0).getUser_id());
                 
 	}
-	//��ȡ��������
+	//
 	public String getBarOwnerName(){
 		return getUserName(String.valueOf(BarInfo.get(0).getUser_id()));
 	}
-	//������id
+	//
 	public String getBarId(){
 		return String.valueOf(BarInfo.get(0).getBar_id());
 	}
-	//��������
+	//
 	public String getBarName(){
 		return BarInfo.get(0).getBar_name();
 	}
 	
-	//����������
+	//
 	public String getBarType(){
 		return BarInfo.get(0).getTopic();
 	}
 	
-	//�����������
+	//
 	public int getPostNum(){
 		return PostList.size();
 	}
 	
-	//�������id
+	//
 	public String getPostId(int index){
 		return String.valueOf(PostList.get(index).getPost_id());
 	}
 	
-	//���¥��id
+	//
 	public String getPosterId(int index){
 		return String.valueOf(PostList.get(index).getUser_id());
 	}
 	
-	//���������
+	//
 	public String getPostName(int index){
 		return String.valueOf(PostList.get(index).getPost_name());
 	}
 	
-	//�����������
+	//
 	public String getPostMsg(int index){
 		return String.valueOf(PostList.get(index).getPost_msg());
 	}
 	
-	//��ȡ����ʱ��
+	//
 	public String getPostDate(int index){
 		return String.valueOf(PostList.get(index).getPost_date());
 	}
 	
-	//��ȡ���ظ�ʱ��
+	//
 	public String getLastDate(int index){
 		return String.valueOf(PostList.get(index).getLast_date());
 	}
 	
-	//��ȡ��󷢲���id
+	//
 	public String getLastId(int index){
 		return String.valueOf(PostList.get(index).getPost_id());
 	}
 	
-	//���ĳ������
+	//
 	public String getUserName(String user_Id){
 		return BarModule.getUserName(user_Id);
 	}
 	
-	//��ȡ�û�ID
+	//
 	public String getUserID(String name){
 		return SearchModule.getUserID(name);
 	}
 	
-	//���ĳ���û��Ƿ��ע�����
+	//
 	public boolean checkStarBar(String user_id){
 		try{
 			if(BarModule.checkStarBar(user_id, getBarId()).equals("1"))
@@ -119,7 +118,7 @@ public class BarCtrl extends Ctrl{
 			else return false;
 		}catch(Exception e) {return false;}
 	}
-	//����Ƿ��Ѿ�ǩ��
+	//
 	public boolean checkSign(String user_id){
 		try{
 			if(BarModule.CheckSign(getBarId(),user_id).equals("1"))
@@ -128,7 +127,7 @@ public class BarCtrl extends Ctrl{
 		}catch(Exception e) {return false;}
 	}
 	
-	//���ĳ���Ƿ�����ʦ
+	//
 	public boolean checkTeacher(String userName){
 		try{
 			String get = BarModule.checkTeacher(this.getUserID(userName));
@@ -140,23 +139,23 @@ public class BarCtrl extends Ctrl{
 			return false;
 		}
 	}
-	//��ȡ��ʦ����
+	//
 	public int getTeacherNum(){
 		return TeacherInfo.size();
 	}
-	//��ȡ��ʦid
+	//
 	public String getTeacherId(int index){
 		return String.valueOf(TeacherInfo.get(index).getUser_id());
 	}
-	//��ȡ��ʦ����
+	//
 	public String getTeacherName(int index){
 		return String.valueOf(TeacherInfo.get(index).getUser_name());
 	}
-	//��ȡĳһ���������
+	//
 	public String getBarName1(String bar_id){
 		return BarModule.getBarName1(bar_id);
 	}
-	//���ĳ�����Ƿ���һ���ɵİ���
+	//
 	public boolean checkBarTeacher(String bar_id,String user_id){
 		try{
 			if(BarModule.checkBarTeacher(user_id, bar_id).equals("1")){
@@ -167,7 +166,7 @@ public class BarCtrl extends Ctrl{
 			return false;
 		}
 	}
-	//��ȡĳ��������id
+	//
 	public String getReplyer(String reply_id){
 		return BarModule.getReplyer(reply_id);
 	}
